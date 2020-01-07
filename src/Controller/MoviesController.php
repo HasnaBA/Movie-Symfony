9e0faa1f2ca2f4a9f2534032b56abe5054618b26
hasna <h.bourajoini@gmail.com>
2020-01-07 15:56:21 +0100

    view film+author+pagination

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request; // Nous avons besoin d'accéder à la requête pour obtenir le numéro de page
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+
 use App\Entity\Movies;
 
 class MoviesController extends AbstractController
@@ -11,17 +14,31 @@ class MoviesController extends AbstractController
     /**
      * @Route("/movies", name="movies")
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginator)
     {
+        $search = (string) $request->query->get('search', null);
 
         $movies = $this->getDoctrine()
         ->getRepository(Movies::class)
-        ->findBy([], ['name' => 'ASC'], 50);
+        ->createQueryBuilder('m')
+        ->where('m.name LIKE :name')
+        ->setParameter('name', '%'.$search.'%')
+        ->orderBy('m.name')
+        ->getQuery()
+        ->execute();
 
+       
+        
+        $movieList = $paginator->paginate(
+            $movies, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            20 // Nombre de résultats par page
+        );
        
 
         return $this->render('movies/index.html.twig', [
             'movies' => $movies,
+            'movieList' => $movieList,
         ]);
     }
     
