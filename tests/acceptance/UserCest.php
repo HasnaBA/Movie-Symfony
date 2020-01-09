12819c1ffceafeba8a461303f0844bd395c4f8e7
hasna <h.bourajoini@gmail.com>
2020-01-09 10:28:10 +0100

    Acceptancetest

---
+++ tests/acceptance/UserCest.php
@@ -0,0 +1,28 @@
+<?php namespace App\Tests;
+use App\Tests\AcceptanceTester;
+
+class UserCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->fillField('email','h.bourajoini@gmail.com');
+        $I->fillField('password','clavie');
+        $I->click('Se connecter');
+        $I->see('LISTE DES FILMS');
+    }
+
+    public function tryToLoginFail(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->fillField('email','h.bourajoini@gmail.com');
+        $I->fillField('password','aaa');
+        $I->click('Se connecter');
+        $I->see('Invalid credentials.');
+    }
+}
